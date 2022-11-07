994d46e4b9f4976ee552d1760716a8d975735c54
Andres <andres_grinblat80@hotmail.com>
2022-11-07 15:13:16 -0300

    desarrollamos componentes de Acerca de

--- src/main/java/com/portfolio/andres/Security/Controller/Mensaje.java
+++ src/main/java/com/portfolio/andres/Security/Controller/Mensaje.java
@@ -1,13 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package com.portfolio.andres.Security.Controller;
 
-/**
- *
- * @author andre
- */
+
 public class Mensaje {
     private String mensaje;
     
