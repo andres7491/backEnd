cab8ddf86a790faaf639472eccff25e2394a4c57
Andres <andres_grinblat80@hotmail.com>
2022-11-08 12:03:44 -0300

    modifique varchar de imagen

--- src/main/java/com/portfolio/andres/Security/Controller/AuthController.java
+++ src/main/java/com/portfolio/andres/Security/Controller/AuthController.java
@@ -38,7 +38,6 @@ import org.springframework.web.bind.annotation.RestController;
 //@CrossOrigin(origins = "http://localhost:4200")
 @CrossOrigin(origins = "https://frontendandres.web.app")
 
-
 public class AuthController {
     @Autowired
     PasswordEncoder passwordEncoder;
